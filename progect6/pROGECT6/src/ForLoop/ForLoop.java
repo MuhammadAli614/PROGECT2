14fd78a997af03fe6231dce8d7f49919fd839bb4
MuhammadAli611 <chma616@gmail.com>
2021-02-17 22:00:08 -0500

    MuhammadAli614 first commit

---
+++ progect6/pROGECT6/src/ForLoop/ForLoop.java
@@ -0,0 +1,10 @@
+package ForLoop;
+
+public class ForLoop {
+	public static void main(String[] args) {
+	for (int k=1; k<=97; k++) {
+		System.out.print("*");
+	}
+	}
+}
+   
---
+++ progect6/pROGECT6/src/ForLoop/Variable.java
@@ -0,0 +1,11 @@
+package ForLoop;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int myNum = 15;
+		int myNum1 = 20;
+		System.out.println(myNum + myNum1);
+	}
+
+}
