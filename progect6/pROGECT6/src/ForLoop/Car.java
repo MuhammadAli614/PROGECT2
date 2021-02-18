6a3a72878bde97930640f14aaedbdfd9ac2af810
MuhammadAli611 <chma616@gmail.com>
2021-02-17 22:00:08 -0500

    first commit

---
+++ progect6/pROGECT6/src/ForLoop/Car.java
@@ -0,0 +1,16 @@
+package ForLoop;
+
+public class Car {
+	public static void main(String[] args) {
+		String CarName = "BMW";
+		String CarModel = "2016";
+		String CarValue = "A";
+		System.out.println("CarModel "+" CarName "+" CarValue ");
+	} 
+	String CarName = "TASLA";
+	String CarModel = "2020";
+	String CarValue = "A+";
+	String CarType = "AutoPilot";
+	
+
+}
